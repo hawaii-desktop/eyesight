1262371b1111fe9812916f5316ab7f72310834f6
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-12-02 10:13:00 +0100

    Update copyright header.

--- src/zoomwidget.h
+++ src/zoomwidget.h
@@ -1,22 +1,29 @@
-/*********************************************************************
-* Copyright (C) 2010 by Dario Ignacio Aguilera                       *
-* dario_21_06@hotmail.com                                            *
-*                                                                    *
-* This program is free software; you can redistribute it and/or      *
-* modify it under the terms of the GNU General Public License        *
-* as published by the Free Software Foundation; either version 2     *
-* of the License, or (at your option) any later version.             *
-*                                                                    *
-* This program is distributed in the hope that it will be useful,    *
-* but WITHOUT ANY WARRANTY; without even the implied warranty of     *
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *
-* GNU General Public License for more details.                       *
-*                                                                    *
-* You should have received a copy of the GNU General Public License  *
-* along with this program; if not, write to the                      *
-* Free Software Foundation, Inc.                                     *
-* 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.      *
-**********************************************************************/
+/****************************************************************************
+ * This file is part of EyeSight.
+ *
+ * Copyright (c) 2012 Pier Luigi Fiorini
+ * Copyright (c) 2010-2011 Dario Ignacio Aguilera <dario_21_06@hotmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * $BEGIN_LICENSE:GPL2+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
 
 #ifndef ZOOMWIDGET_H
 #define ZOOMWIDGET_H
